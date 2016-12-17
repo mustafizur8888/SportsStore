cfe7604579b4f4331e4fa43f84d89e9e7dd1758c
Mustafizur Rahman <Mustafizur Rahman@MUSTAFIZUR>
2016-12-18 00:29:34 +0600

    Add project files.

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+        public ActionResult List(string category, int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products =
+                category == null ?
+                repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize)
+                : repository.Products.Where(p => p.Category == null || p.Category == category).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Count(x => x.Category == category)
+                },
+                CurrentCategory = category
+            };
+
+
+            return View(model);
+        }
+    }
+}
